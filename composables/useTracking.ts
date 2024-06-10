e0df4774e53c3505164d79ba4559f0c8986f78a2
Soheil <soheil.nazari@check24.de>
2024-06-10 08:16:33 +0200

    add no_tracking param nt

--- composables/useTracking.ts
+++ composables/useTracking.ts
@@ -4,12 +4,21 @@ export default function useTracking(componentName: string): {
   track: (trackingName: string) => void
   trackAndNavigate: (trackingName: string, href: string) => void
 } {
+  const trackingCookie = useCookie('nt')
+  const trackingEnabled = trackingCookie.value === 'true'
+
   return {
     track: (trackingName: string) => {
-      track(componentName + ' - ' + trackingName)
+      if (trackingEnabled) {
+        track(componentName + ' - ' + trackingName)
+      }
     },
+
     trackAndNavigate: (trackingName: string, href: string) => {
-      track(componentName + ' - ' + trackingName)
+      if (trackingEnabled) {
+        track(componentName + ' - ' + trackingName)
+      }
+
       navigateTo(href)
     },
   }
